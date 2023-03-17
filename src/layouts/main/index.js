3eee0804982a15a3d2a281224483d6ddf4d28efb
Akash <ag308669@gmail.com>
2023-03-17 12:55:34 +0530

    Added the login route and created the main layout

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Container, Stack } from '@mui/material';
+import logo from '../../assets/Images/logo.png';
 
 const MainLayout = () => {
   return (
-    <>
-      <div>Main Layout</div>
+    <Container sx={{ mt: 5, maxWidth: 'sm' }}>
+      <Stack spacing={20}>
+        <Stack sx={{ width: '100%' }} direction="column" alignItems="center">
+          <img style={{ height: 100, width: 100 }} src={logo} alt="logo" />
+        </Stack>
+      </Stack>
 
       <Outlet />
-    </>
+    </Container>
   );
 };
 
