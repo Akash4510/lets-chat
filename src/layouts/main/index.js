c3597765a3c27c8e54143d8021abad899090037b
Akash <ag308669@gmail.com>
2023-03-18 13:06:03 +0530

    Added bottom margin to the logo in main layout

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -7,7 +7,11 @@ const MainLayout = () => {
   return (
     <Container sx={{ mt: 5 }} maxWidth="sm">
       <Stack spacing={20}>
-        <Stack sx={{ width: '100%' }} direction="column" alignItems="center">
+        <Stack
+          sx={{ width: '100%', mb: '40px' }}
+          direction="column"
+          alignItems="center"
+        >
           <img style={{ height: 100, width: 100 }} src={logo} alt="logo" />
         </Stack>
       </Stack>
