c58cf16d0c3e55e6c756eb20bada5fdc567dbdb8
Akash <ag308669@gmail.com>
2023-04-18 15:47:06 +0530

    Minor changes on user object

--- src/redux/slices/app.js
+++ src/redux/slices/app.js
@@ -261,7 +261,17 @@ export const UpdateUserProfile = (formValues) => {
       )
       .then((response) => {
         console.log(response);
-        dispatch(slice.actions.setUser({ user: response.data.data }));
+        const user = response.data.data;
+        dispatch(
+          slice.actions.setUser({
+            user: {
+              _id: user._id,
+              firstName: user.firstName,
+              lastName: user.lastName,
+              email: user.email,
+            },
+          })
+        );
       })
       .catch((err) => {
         console.log(err);
